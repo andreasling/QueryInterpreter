46cd547631096ed0d5cfb2c1ba8e68e82df2d835
Andreas Ling <andreas.ling@universum.se>
2013-03-29 22:08:07 +0100

    adding query parser; boolean literal

---
+++ QueryInterpreter.Tests/ParserTests/ParserTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using QueryInterpreter.Expressions;
+using QueryInterpreter.Parser;
+
+namespace QueryInterpreter.Tests.ParserTests
+{
+    [TestFixture]
+    public class ParserTests
+    {
+        [Test]
+        public void ShouldParseTrueLiteral()
+        {
+            Assert.AreEqual(
+                new BinaryLiteralExpression(true),
+                new QueryParser("true").Parse()
+                );
+        }
+    }
+}
---
+++ QueryInterpreter/Parser/QueryParser.cs
@@ -0,0 +1,19 @@
+﻿using QueryInterpreter.Expressions;
+
+namespace QueryInterpreter.Parser
+{
+    public class QueryParser
+    {
+        private readonly string query;
+
+        public QueryParser(string query)
+        {
+            this.query = query;
+        }
+
+        public Expression Parse()
+        {
+            return new BinaryLiteralExpression(bool.Parse(query));
+        }
+    }
+}
