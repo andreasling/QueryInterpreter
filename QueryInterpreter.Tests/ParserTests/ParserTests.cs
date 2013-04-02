d9c11cec4a7f77ac3f1f2abd64ea7e2e53fb3c17
Andreas Ling <andreas.ling@universum.se>
2013-04-02 17:40:32 +0200

    working dir

--- QueryInterpreter.Tests/ParserTests/ParserTests.cs
+++ QueryInterpreter.Tests/ParserTests/ParserTests.cs
@@ -72,6 +72,22 @@ namespace QueryInterpreter.Tests.ParserTests
                 new EqualsExpression(new BooleanLiteralExpression(true), new BooleanLiteralExpression(true)),
                 new QueryParser("true=true").Parse());
         }
+
+        [Test]
+        public void ShouldParseComplexExpression()
+        {
+            var actual = new QueryParser("not true=(not \"\"= \"hello\" or false or true) and \"a = b\" =\"a = b\"").Parse();
+            var expected = 
+                new NotExpression(
+                    new EqualsExpression(
+                        new BooleanLiteralExpression(true), 
+                        new OrExpression(
+                            new NotExpression(
+                                new EqualsExpression(
+                                    new StringLiteralExpression("hello"), 
+                                    new StringLiteralExpression("hello"))),
+
+        }
     }
 }
 
